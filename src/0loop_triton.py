94c9889332fcf1da310bf1a075e46cda3a436f98
Najwa Laabid <najwalaabid@aalto.fi>
2023-05-25 11:10:49 +0300

    updating src from triton

--- src/0loop_triton.py
+++ src/0loop_triton.py
@@ -20,7 +20,7 @@ jobids_file = os.path.join(job_directory, 'jobids.txt')
 mkdir_p(job_directory)
 mkdir_p(output_dir)
 
-args_ = ['uspto50k'] # types of losses
+args_ = ['qm9_no_h'] 
 
 for arg in args_:
     print(f"Creating job {arg}... ")
@@ -37,16 +37,17 @@ for arg in args_:
         fh.writelines(f"#SBATCH --output={output_dir}/{arg}_%a.out\n")
         fh.writelines(f"#SBATCH --error={output_dir}/{arg}_%a.err\n")
         fh.writelines("#SBATCH --partition=gpu\n")
-        fh.writelines("#SBATCH --gres=gpu:v100:1\n")
+        fh.writelines("#SBATCH --constraint=ampere\n")
+        fh.writelines("#SBATCH --gres=gpu:a100:1\n")
         fh.writelines("#SBATCH --cpus-per-task=4\n")
         fh.writelines("#SBATCH --mem-per-cpu=10G\n")
         fh.writelines("#SBATCH --array=1-1\n")
         fh.writelines("#SBATCH --mail-type=ALL\n")
         fh.writelines("#SBATCH --mail-user=najwa.laabid@aalto.fi\n")
-        fh.writelines("#SBATCH --time=01:00:00\n")
+        fh.writelines("#SBATCH --time=15:00:00\n")
         fh.writelines("module load miniconda\n")
         fh.writelines(f"source activate {conda_env}\n\n")
-        fh.writelines(f"python3 {script_name} +experiment={arg}.yaml"+
+        fh.writelines(f"python3 {script_name} +experiment={arg}.yaml dataset=qm9"+
                       f" hydra.run.dir=../experiments/mol/trained_models/{arg}\n")
 
     result = subprocess.run(args="sbatch", stdin=open(job_file, 'r'), capture_output=True)
